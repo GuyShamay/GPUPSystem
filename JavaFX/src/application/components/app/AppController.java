ee7bafff02392961670eb36cadcc0b64ed8074b5
NoamGaloz <Noam@galoz.com>
2022-01-01 13:43:37 +0200

    AppController fxml

--- JavaFX/src/application/components/app/AppController.java
+++ JavaFX/src/application/components/app/AppController.java
@@ -1,4 +1,41 @@
 package application.components.app;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class AppController {
+
+    @FXML
+    private Button buttonLoadFile;
+
+    @FXML
+    private Button buttonInfo;
+
+    @FXML
+    private Button buttonActions;
+
+    @FXML
+    private Button buttonTask;
+
+    @FXML
+    void buttonActionsClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonInfoClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonLoadFileClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonTaskClicked(ActionEvent event) {
+
+    }
 }
