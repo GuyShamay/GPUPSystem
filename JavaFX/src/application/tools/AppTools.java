abf050464f39d0f73d7bc5fe4133f78baa30ecf0
Guy Shamay <guysy@mta.ac.il>
2022-01-01 15:06:26 +0200

    load file button

---
+++ JavaFX/src/application/tools/AppTools.java
@@ -0,0 +1,13 @@
+package application.tools;
+
+import javafx.scene.control.Alert;
+
+public abstract class AppTools {
+    public static void warningAlert(String header, String msg, String instructions){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(header);
+        alert.setHeaderText(msg);
+        alert.setContentText(instructions);
+        alert.showAndWait();
+    }
+}
